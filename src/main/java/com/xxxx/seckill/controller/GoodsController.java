942dc8f8cd63d727bde1547010717636b600a6c5
jiayuqi <541443699@qq.com>
2022-09-24 14:57:15 +0800

    24.秒杀倒计时处理

--- src/main/java/com/xxxx/seckill/controller/GoodsController.java
+++ src/main/java/com/xxxx/seckill/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.xxxx.seckill.controller;
 import com.xxxx.seckill.pojo.User;
 import com.xxxx.seckill.service.IGoodsService;
 import com.xxxx.seckill.service.IUserService;
+import com.xxxx.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * Date:2022/9/23 10:21
@@ -51,7 +53,29 @@ public class GoodsController {
     @RequestMapping("/toDetail/{goodsId}")
     public String toDetail(Model model, User user, @PathVariable Long goodsId) {
         model.addAttribute("user", user);
-        model.addAttribute("goods", goodsService.findGoodsVoByGoodsId(goodsId));
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态
+        int secKillStatus = 0;
+        //秒杀倒计时
+        int remainSeconds = 0;
+        // 秒杀还未开始
+        if (nowDate.before(startDate)) {
+            remainSeconds = (int) ((startDate.getTime() - nowDate.getTime()) / 1000);
+        } else if (nowDate.after(endDate)) {
+            // 秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            // 秒杀进行中
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("remainSeconds", remainSeconds);
+        model.addAttribute("secKillStatus", secKillStatus);
+        model.addAttribute("goods", goodsVo);
         return "goodsDetail";
     }
 }
